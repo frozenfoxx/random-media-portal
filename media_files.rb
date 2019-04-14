a18886f79b39de4b75deb923c48d61b02365925a
frozenfoxx <frozenfoxx@churchoffoxx.net>
2019-04-13 18:53:11 -0700

    Broke out media_files

---
+++ media_files.rb
@@ -0,0 +1,20 @@
+module MediaFiles
+    def initialize(directory)
+        @directory = directory
+        @file_list = []
+        update_files
+    end
+
+    def update_files
+        # Update the internal @file_list
+
+        @file_list = []
+
+        Dir.foreach(@directory) do |file|
+            next if file == '.' or file == '..'
+            @file_list.push(file)
+        end
+    end
+
+    attr_reader :file_list
+end
