8a2fbe4f31d1aa03e799947435535c5422aa6963
frozenfoxx <frozenfoxx@churchoffoxx.net>
2019-04-24 00:16:14 -0700

    Removed variables from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env ash
 
 HOST=${HOST:-"0.0.0.0"}
-MEDIA_DIRECTORY=${MEDIA_DIRECTORY:-"/data"}
-MEDIA_MODE=${MEDIA_MODE:-"video"}
 PORT=${PORT:-"4567"}
 
 rackup -o ${HOST} -p ${PORT} $@
