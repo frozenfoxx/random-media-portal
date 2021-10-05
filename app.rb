51932e86d68a8a0fc35b5b9866ee1ef679b9b52a
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2021-10-05 00:57:16 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,39 +3,51 @@ require_relative './media_directory'
 
 class RandomMediaPortal < Sinatra::Base
 
-    configure do
-        set :bind, ENV["HOST"]
-        set :media_mode, ENV["MEDIA_MODE"]
-        set :port, ENV["PORT"]
-        set :root, File.dirname(__FILE__)
-        set :public_folder, ENV["MEDIA_DIRECTORY"]
-        enable :logging
-        file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
-        file.sync = true
-    end
+  configure do
+    set :bind, ENV["HOST"]
+    set :media_mode, ENV["MEDIA_MODE"]
+    set :port, ENV["PORT"]
+    set :root, File.dirname(__FILE__)
+    set :public_folder, ENV["MEDIA_DIRECTORY"]
+    enable :logging
+    file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+    file.sync = true
+  end
 
-    # Default route
-    get '/*' do
-        media_directory = MediaDirectory.new(settings.public_folder)
+  # Default route
+  get '/*' do
+    media_directory = MediaDirectory.new(settings.public_folder)
 
-        # Return a view based on mode
-        case settings.media_mode
-        when 'audio'
-            @media_file = media_directory.random_audio
-            @media_extension = @media_file.split('.').last
-            erb :audio
-        when 'image'
-            @media_file = media_directory.random_image
-            erb :image
-        when 'video'
-            @media_file = media_directory.random_video
-            @media_extension = @media_file.split('.').last
-            erb :video
+    # Return a view based on mode
+    case settings.media_mode
+      when 'audio'
+        if @media_file = media_directory.random_audio
+          @media_extension = @media_file.split('.').last
+          erb :audio
         else
-            status 500
-            body "Unsupported media mode: #{settings.media_mode}"
+          status 404
+          body "File not found"
         end
+      when 'image'
+        if @media_file = media_directory.random_image
+          erb :image
+        else
+          status 404
+          body "File not found"
+        end
+      when 'video'
+        if @media_file = media_directory.random_video
+          @media_extension = @media_file.split('.').last
+          erb :video
+        else
+          status 404
+          body "File not found"
+        end
+      else
+        status 500
+        body "Unsupported media mode: #{settings.media_mode}"
+      end
     end
 
-    run! if __FILE__ == $0
+  run! if __FILE__ == $0
 end
