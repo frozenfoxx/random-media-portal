ca35b8685f1ef8f0fc9330b11c2398572d81137b
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2021-10-05 01:01:08 -0700

    Update media_directory.rb

--- media_directory.rb
+++ media_directory.rb
@@ -1,74 +1,77 @@
 class MediaDirectory
-    def initialize(directory)
-        @directory = directory
-        @modes = [
-            'audio',
-            'image',
-            'video'
-        ]
+  def initialize(directory)
+    @directory = directory
+    @modes = [
+      'audio',
+      'image',
+      'video'
+    ]
+  end
+
+  def audio_files
+    # Return a list of all audio files
+
+    audio_types = "mp3,ogg,wav"
+    Dir["#{@directory}/*.{#{audio_types}}"]
+  end
+
+  def image_files
+    # Return a list of all image files
+
+    image_types = "gif,jpg,jpeg,png"
+    Dir["#{@directory}/*.{#{image_types}}"]
+  end
+
+  def has_mode?(mode)
+    # Checks if a mode is supported
+
+    @modes.member?(mode)
+  end
+
+  def random_audio
+    # Returns a random audio file from the directory
+
+    if audio_files.size >= 1
+      choice = rand(0..audio_files.size-1)
+      strip_quotes(File.basename(audio_files[choice]))
     end
+  end
 
-    def audio_files
-        # Return a list of all audio files
+  def random_image
+    # Returns a random image file from the directory
 
-        audio_types = "mp3,ogg,wav"
-        Dir["#{@directory}/*.{#{audio_types}}"]
+    if image_files.size >= 1
+      choice = rand(0..image_files.size-1)
+      strip_quotes(File.basename(image_files[choice]))
     end
+  end
 
-    def image_files
-        # Return a list of all image files
-
-        image_types = "gif,jpg,jpeg,png"
-        Dir["#{@directory}/*.{#{image_types}}"]
+  def random_video
+    # Returns a random video file from the directory
+ 
+    if video_files.size >= 1
+      choice = rand(0..video_files.size-1)
+      strip_quotes(File.basename(video_files[choice]))
     end
+  end
 
-    def has_mode?(mode)
-        # Checks if a mode is supported
+  def strip_quotes(quoted_string)
+    # Takes a string and strips the quotes from it
+    # Params
+    # +quoted_string+:: a media file string with quotes
 
-        @modes.member?(mode)
-    end
+    quoted_string.sub(/^\"/, '').sub(/\"$/, '')
 
-    def random_audio
-        # Returns a random audio file from the directory
+    # In Ruby 2.5+ this method is preferable
+    # quoted_string.delete_prefix('"').delete_suffix('"')
+  end
 
-        choice = rand(0..audio_files.size-1)
+  def video_files
+    # Return a list of all video files
 
-        strip_quotes(File.basename(audio_files[choice]))
-    end
+    video_types = "mp4,ogg,webm"
+    Dir["#{@directory}/*.{#{video_types}}"]
+  end
 
-    def random_image
-        # Returns a random image file from the directory
-
-        choice = rand(0..image_files.size-1)
-
-        strip_quotes(File.basename(image_files[choice]))
-    end
-
-    def random_video
-        # Returns a random video file from the directory
-
-        choice = rand(0..video_files.size-1)
-
-        strip_quotes(File.basename(video_files[choice]))
-    end
-
-    def strip_quotes(quoted_string)
-        # Takes a string and strips the quotes from it
-        # Params
-        # +quoted_string+:: a media file string with quotes
-
-        quoted_string.sub(/^\"/, '').sub(/\"$/, '')
-
-        # In Ruby 2.5+ this method is preferable
-        # quoted_string.delete_prefix('"').delete_suffix('"')
-    end
-
-    def video_files
-        # Return a list of all video files
-
-        video_types = "mp4,ogg,webm"
-        Dir["#{@directory}/*.{#{video_types}}"]
-    end
-
-    attr_reader :modes
+  attr_reader :modes
 end
